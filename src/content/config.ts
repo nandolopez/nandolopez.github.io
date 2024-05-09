25d915b07f1f4977fc3736f402286bd4bc6bf51f
Nando López <fernanlm88@gmail.com>
2024-05-09 23:08:20 +0200

    fixed issue with dynamic assignation

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,25 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		language: z.string(),
-		index: z.boolean(),
-		publication_date: z.coerce.date(),
-		update_date: z.coerce.date().optional(),
-		thumbnail: z.string().optional(),
-		meta_title: z.string().optional(),
-		meta_description: z.string().optional(),
-		topic: z.string().optional(),
-		status: z.string().optional(),
-		tags: z.string().optional()
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    language: z.string(),
+    index: z.boolean(),
+    publication_date: z.coerce.date(),
+    update_date: z.coerce.date().optional(),
+    thumbnail: z.string().optional(),
+    meta_title: z.string().optional(),
+    meta_description: z.string().optional(),
+    topic: z.string().optional(),
+    status: z.string().optional(),
+    tags: z.string().optional(),
+  }),
 });
 
 export const collections = {
-	"blog": blogCollection,
-	"projects": blogCollection
+  blog: blogCollection,
+  projects: blogCollection,
 };
