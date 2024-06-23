f9ea44fb1d52b0e0751c32ddb0369c1c3e94d526
Fernando López de Lerma Martínez <fernanlm88@gmail.com>
2024-06-23 22:14:05 +0200

    modified contact for smart links

--- src/db/DB_SocialMedia.ts
+++ src/db/DB_SocialMedia.ts
@@ -3,42 +3,49 @@ type SocialMedia = {
   icon: string;
   inFooter: boolean;
   link: string;
+  smartlink: string;
 };
 
 export const DB_SocialMedia: SocialMedia[] = [
   {
     alt: "Github",
     link: "https://github.com/nandolopez/",
+    smartlink: "https://github.com/nandolopez/",
     icon: "/svg_social/Github.svg",
     inFooter: true,
   },
   {
     alt: "Instagram",
-    link: "https://www.instagram.com/nandolopez_tech/",
+    link: "https://www.instagram.com/nandolopez_tech",
+    smartlink: "instagram://user?username=nandolopez_tech",
     icon: "/svg_social/Instagram.svg",
     inFooter: true,
   },
   {
     alt: "Linkedin",
-    link: "hhttps://www.linkedin.com/in/fernandollm/",
+    link: "https://www.linkedin.com/in/fernandollm/",
+    smartlink: "linkedin://in/fernandollm",
     icon: "/svg_social/Linkedin.svg",
     inFooter: false,
   },
   {
-    alt: "Spotify list",
-    link: "https://www.youtube.com/channel/UCg4pdmmXX07n89Axjm41Gqw",
+    alt: "Spotify",
+    link: "https://open.spotify.com/user/fermaxando",
+    smartlink: "spotify:user:fermaxando",
     icon: "/svg_social/Spotify.svg",
     inFooter: false,
   },
   {
     alt: "Tiktok Channel",
     link: "https://www.tiktok.com/@fernanlm88",
+    smartlink: "tiktok://tiktok.com/@fernanlm88",
     icon: "/svg_social/Tiktok.svg",
     inFooter: false,
   },
   {
     alt: "Youtube Channel",
     link: "https://www.youtube.com/channel/UCg4pdmmXX07n89Axjm41Gqw",
+    smartlink: "vnd.youtube://channel/UCg4pdmmXX07n89Axjm41Gqw",
     icon: "/svg_social/Youtube.svg",
     inFooter: true,
   },
@@ -46,6 +53,7 @@ export const DB_SocialMedia: SocialMedia[] = [
   {
     alt: "X / Twitter",
     link: "https://www.twitter.com",
+    smartlink: "twitter://user?screen_name=NandoLopez_Tech",
     icon: "/svg_social/X.svg",
     inFooter: false,
   },
